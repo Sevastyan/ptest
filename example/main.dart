04e5c6f76c5361b3de29e18e677dcb0c7e70acbd
Sevastyan Savanyuk <sevasnex@gmail.com>
2025-03-20 11:02:19 +0100

    feat: Add random set

--- example/main.dart
+++ example/main.dart
@@ -3,14 +3,14 @@ import 'package:test/test.dart';
 
 void main() {
   pTest(
-    'Example of a test.',
+    'Example of a test for integer.',
     generateInput: (random) => random.int(),
     body: (input) {
       final sut = toTestFun;
 
-      expect(sut(input), input +1);
+      expect(sut(input), input + 1);
     },
   );
 }
 
-int toTestFun(int input) => /*input == 934 ? 934 :*/ input + 1;
+int toTestFun(int input) => input + 1;
--- lib/src/random_extensions.dart
+++ lib/src/random_extensions.dart
@@ -3,4 +3,6 @@ import 'dart:math';
 
 extension RandomExtensions on Random {
   dart.int int({dart.int? max}) => nextInt(max ?? 1000);
+
+  T fromSet<T>(dart.Set<T> set) => set.elementAt(nextInt(set.length));
 }
